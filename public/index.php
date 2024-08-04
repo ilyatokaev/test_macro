89c67e9080fd9e7a913309ed65576b5c4e4bbfc8
ustahot <info.iluha@gmail.com>
2024-08-04 22:55:45 +0300

    transfer_commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+// Register the Composer autoloader...
+require __DIR__.'/../vendor/autoload.php';
+//require __DIR__.'/../app/Controllers/Controller.php';
+
+$controller = new \App\Controllers\Controller();
+//$controller->index();
+echo  '465456465465465456';
